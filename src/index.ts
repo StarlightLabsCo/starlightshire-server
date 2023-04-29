02f677df946d689083e9ee5546682c8b2d56c75c
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-04-29 13:29:50 -0700

    Init websocket server repo

---
+++ src/index.ts
@@ -0,0 +1,101 @@
+// Environment Variables
+import dotenv from "dotenv";
+dotenv.config();
+
+// Webs Sockets
+import { WebSocketServer } from "ws";
+
+if (!process.env.PORT) {
+    console.error("PORT environment variable not set");
+    process.exit(1);
+}
+const wss = new WebSocketServer({ port: process.env.PORT as any as number });
+const clients = new Map();
+
+import { randomUUID } from "crypto";
+
+export type ConnectionMetadata = {
+    id: string;
+};
+
+export type StreamMessage = {
+    type: string;
+    data: any;
+};
+
+// Starlight functions
+// import {
+//     startAudioCaptureStream,
+//     handleAudioCaptureStreamData,
+//     endAudioCaptureStream,
+// } from "./audioCapture.js";
+// import {
+//     startConversation,
+//     endConversation,
+//     continueConversation,
+//     startNPCConversations,
+// } from "./conversation.js";
+
+const handlers = {
+    // AudioCaptureStreamStart: startAudioCaptureStream,
+    // AudioCaptureStreamData: handleAudioCaptureStreamData,
+    // AudioCaptureStreamEnd: endAudioCaptureStream,
+    // ConversationStart: startConversation,
+    // ConversationContinue: continueConversation,
+    // ConversationEnd: endConversation,
+};
+
+wss.on("connection", (ws) => {
+    // Init Connection
+    console.log("WebSocket connection established");
+
+    const connectionMetadata: ConnectionMetadata = {
+        id: randomUUID(),
+    };
+
+    clients.set(ws, connectionMetadata);
+
+    ws.send(
+        JSON.stringify({
+            type: "ConnectionEstablished",
+            data: connectionMetadata.id,
+        })
+    );
+
+    // Define event handlers
+    ws.on("message", async (message) => {
+        // Check if message is valid
+        if (
+            !message ||
+            message == undefined ||
+            message.toString().length === 0
+        ) {
+            console.log("No message received. Skipping...");
+            return;
+        }
+
+        // Parse message
+        const streamMessage = JSON.parse(message.toString()) as StreamMessage;
+
+        // Check if parsed message is valid
+        if (!streamMessage.type) {
+            console.log("No type received. Skipping...");
+            return;
+        }
+
+        if (!handlers[streamMessage.type]) {
+            console.log("No handler found. Skipping...");
+            return;
+        }
+
+        // Handle message
+        await handlers[streamMessage.type](ws, streamMessage.data);
+    });
+
+    ws.on("close", () => {
+        console.log(`connection (id = ${clients.get(ws).id}) closed`);
+        clients.delete(ws);
+    });
+});
+
+export { wss, clients };
