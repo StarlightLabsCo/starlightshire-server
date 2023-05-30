43e5e69e2a847e6864543f83837c6c48154a2811
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-05-29 21:15:13 -0700

    Add action history

--- src/ai/agent.ts
+++ src/ai/agent.ts
@@ -1,6 +1,8 @@
 import { Action } from "../actions.js";
 import { createChatCompletion } from "./openai.js";
 
+const history = [];
+
 async function getAction(
     ws: WebSocket,
     data: {
@@ -21,47 +23,85 @@ async function getAction(
     prompt += `- Name: Thomas Smith` + "\n";
     prompt += `- Age: 25` + "\n";
     prompt += `- Occupation: Lumberjack` + "\n";
-    prompt += `- Personality: Introverted, Shy, Kind, Hardworking` + "\n";
-    prompt += `Location: ${data.location.x}, ${data.location.y}\n`;
+    prompt += `- Personality: Introverted, Shy, Kind, Hardworking` + "\n\n";
+    prompt += `Location: ${data.location.x}, ${data.location.y}\n\n`;
     prompt += `Environment:\n`;
     for (let i = 0; i < data.environment.length; i++) {
         const environment = data.environment[i];
         prompt += `- ${environment}\n`;
     }
-    prompt += `Inventory:\n`;
-    if (data.inventory.length === 0) {
-        prompt += `- Nothing\n`;
-    } else {
+    prompt += "\n";
+
+    if (data.inventory.length > 0) {
+        prompt += `Inventory:\n`;
         for (let i = 0; i < data.inventory.length; i++) {
             const item = data.inventory[i];
             prompt += `- ${item}\n`;
         }
+        prompt += "\n";
     }
-    prompt += `Available Actions (you can do these right now!):\n`;
+
+    prompt += `Available Actions:\n`;
     for (let i = 0; i < data.availableActions.length; i++) {
         const action = data.availableActions[i];
-        prompt += `- ${action}\n`;
+        prompt += `- ${action}\n\n`;
     }
-    prompt += `Hitbox (what you would hit with a swing based action):\n`;
-    if (data.hitbox.length === 0) {
-        prompt += `- Nothing\n`;
-    } else {
+    prompt += "\n";
+
+    if (data.hitbox.length > 0) {
+        prompt += `Hitbox (what you would hit with a swing based action):\n`;
         for (let i = 0; i < data.hitbox.length; i++) {
             const hitbox = data.hitbox[i];
             prompt += `- ${hitbox}\n`;
         }
+        prompt += "\n";
     }
 
-    prompt += `Task: \n`;
+    prompt += `Task List: \n`;
     prompt += `- Find and pick up all the wood possible.\n`;
     prompt += "- Store any extra wood in the chest.\n";
     prompt += "\n";
 
-    prompt += `Given the available actions, and accounting for pathfinding limits (no exact location reach, consider <0.5m as destination) and hitbox rules, which should Thomas take? Respond in JSON: { type: [ActionType], data: {characterId optional parameters}}. Optional parameters: 'x', 'y' for MoveTo, 'itemId' for PickUpItem. Please note that if an action is in the available items list, you can carry out that action right now, without needing to change or move. (e.g. if PickUpItem is in available actions, you can pick up at that item by creating a PickUpItem json object.) First you should list your reasoning and create a plan, and then using that plan, select an action and create a JSON object for that action with the necessary info. The JSON object must be immediately after "Action: " as we're using regex to parse it.\n`;
+    if (history.length > 0) {
+        prompt += `Previous Actions:\n`;
+        // print the most recent 5 actions
+        for (let i = Math.max(0, history.length - 5); i < history.length; i++) {
+            const action = history[i];
+            prompt += `- [${action.type}]: ${JSON.stringify(action.data)}\n`;
+        }
+        prompt += "\n";
+    }
+
+    prompt += `Given the available actions and the assigned task, which should Thomas take? Respond in JSON: { type: [ActionType], data: {characterId optional parameters}}. Please note that if an action is in the available items list, you can execute it immediately, without needing to change or move. (e.g. if PickUpItem is in available actions, you can pick up that item by creating a PickUpItem json object.) First you should list your reasoning and create a plan, and then using that plan, select an action and create a JSON object for that action with the necessary info. The JSON object must be immediately after "Action: " as we're using regex to parse it.\n\n`;
+
+    const extractJSON = (str) => {
+        let firstOpen, firstClose, candidate;
+        firstOpen = str.indexOf("{");
+        do {
+            firstClose = str.lastIndexOf("}");
+            if (firstClose <= firstOpen) {
+                return null;
+            }
+            do {
+                candidate = str.substring(firstOpen, firstClose + 1);
+                try {
+                    const result = JSON.parse(candidate);
+                    return result;
+                } catch (e) {
+                    console.log("Failed to parse " + candidate);
+                }
+                firstClose = str.substr(0, firstClose).lastIndexOf("}");
+            } while (firstClose > firstOpen);
+            firstOpen = str.indexOf("{", firstOpen + 1);
+        } while (firstOpen !== -1);
+    };
 
     let generationAttempts = 0;
     while (generationAttempts < 5) {
         try {
+            console.log("--- Prompt ---");
+            console.log(prompt);
+
             const response = await createChatCompletion([
                 {
                     role: "user",
@@ -73,21 +113,20 @@ async function getAction(
                 },
             ]);
 
-            console.log("--- Prompt ---");
-            console.log(prompt);
-
             console.log("--- Response ---");
             console.log("Plan: ");
             console.log(response);
 
-            // Parse the response
-            // Use regex to get evrything after "Action: "
-            const action = response.match(/Action: (.*)/)[1].trim();
+            const actionJSON = extractJSON(response);
 
-            const actionJSON = JSON.parse(action);
+            if (actionJSON === null) {
+                throw new Error("No valid JSON object found in response");
+            }
 
             // Verify action schema
             const verifiedAction = Action.parse(actionJSON);
+
+            history.push(verifiedAction);
 
             ws.send(JSON.stringify(verifiedAction));
 
--- src/ai/openai.ts
+++ src/ai/openai.ts
@@ -5,19 +5,26 @@ dotenv.config();
 
 import config from "../config.json" assert { type: "json" };
 
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+//     basePath: "https://api.openai.withlogging.com/v1",
+//     baseOptions: {
+//         headers: {
+//             "X-Api-Key": `Bearer ${process.env.LLM_REPORT_API_KEY}`,
+//         },
+//     },
+// });
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-    basePath: "https://api.openai.withlogging.com/v1",
-    baseOptions: {
-        headers: {
-            "X-Api-Key": `Bearer ${process.env.LLM_REPORT_API_KEY}`,
-        },
-    },
+    basePath: "https://api.openai.com/v1",
 });
 
 const openai = new OpenAIApi(configuration);
 
 async function getEmbedding(input: string) {
+    console.log("[OPENAI] Creating embedding...");
+
     let requestAttempts = 0;
 
     while (requestAttempts < config.requestAttempts) {
@@ -36,14 +43,30 @@ async function getEmbedding(input: string) {
 }
 
 async function createChatCompletion(messages: ChatCompletionRequestMessage[]) {
-    let requestAttempts = 0;
+    console.log("[OPENAI] Creating chat completion...");
 
+    let requestAttempts = 0;
     while (requestAttempts < config.requestAttempts) {
         try {
+            const startTime = performance.now();
+
             const response = await openai.createChatCompletion({
                 model: config.model,
                 messages: messages,
             });
+
+            const endTime = performance.now();
+
+            console.log(
+                `[OPENAI] Chat completion took ${endTime - startTime}ms`
+            );
+
+            const generatedTokens = response.data.usage.completion_tokens;
+
+            console.log(
+                "[OPENAI] Tokens per second: " +
+                    generatedTokens / ((endTime - startTime) / 1000)
+            );
 
             return response.data.choices[0].message.content.trim();
         } catch (e) {
