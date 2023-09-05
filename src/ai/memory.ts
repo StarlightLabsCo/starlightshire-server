23d6688c38c63b72a2a632f9d393c5c78f2fb1e0
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-09-04 19:17:42 -0700

    Overhaul memory function.

--- src/ai/memory.ts
+++ src/ai/memory.ts
@@ -1,7 +1,8 @@
-import { prisma } from "../db.js";
-import { Character, Memory } from "@prisma/client";
 import colors from "colors";
+import { Character, Memory } from "@prisma/client";
+import pgvector from "pgvector/utils";
 
+import { prisma } from "../db.js";
 import { createChatCompletion, getEmbedding } from "./openai.js";
 import { generateReflection } from "./reflection.js";
 
@@ -34,20 +35,6 @@ const getMemoryImportance = async (character: Character, memory: string) => {
 
     // Convert from string to number
     return Number(response.content.trim());
-};
-
-const cosineSimilarity = (a: number[], b: number[]) => {
-    let dotProduct = 0;
-    let normA = 0;
-    let normB = 0;
-
-    for (let i = 0; i < a.length; i++) {
-        dotProduct += a[i] * b[i];
-        normA += a[i] * a[i];
-        normB += b[i] * b[i];
-    }
-
-    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
 };
 
 const createMemory = async (
@@ -167,112 +154,73 @@ const getRelevantMemories = async (
     top_k: number,
     updateAccessedAt: boolean = true
 ) => {
-    // Get the embedding of the query
     const queryEmbedding = await getEmbedding(
         query,
         "getRelevantMemoriesQuery",
         character.id
     );
 
-    // const memories = await prisma.memory.findMany({
-    //     where: {
-    //         characterId: character.id,
-    //     },
-    // });
+    const queryEmbeddingSql = pgvector.toSql(queryEmbedding);
 
     const memories = (await prisma.$queryRaw`
-        SELECT * FROM "Memory" WHERE "characterId" = ${character.id}
-    `) as (Memory & {
-        embedding: number[];
-    })[];
-
-    // Noramlize the memories based on their similarity, importance, and recency
-    let similarityMin: number = Number.MAX_VALUE;
-    let similarityMax: number = Number.MIN_VALUE;
-
-    let recencyMin: number = Number.MAX_VALUE;
-    let recencyMax: number = Number.MIN_VALUE;
-
-    let importanceMin: number = Number.MAX_VALUE;
-    let importanceMax: number = Number.MIN_VALUE;
-
-    //  Time used for calculating exponential decay of recency. We save it here so it's the same for all memories
-    const currentTime = Date.now();
-
-    // Also add the similarity to the query (so it doesn't have to be calculated again)
-    const memoriesWithSimilarity = memories.map((memory) => {
-        // Similarity
-        const similarity = cosineSimilarity(queryEmbedding, memory.embedding);
-
-        // In our implementation, we treat recency as an exponential decay function
-        // over the number of sandbox game hours since the memory was
-        // last retrieved. Our decay factor is 0.99.
-        const recency = Math.pow(
-            0.99,
-            (currentTime - memory.accessedAt.getTime()) / 1000 / 60 / 60 /// turn into hours
-        );
-
-        // Mark the min and max values for each to normalize later
-        similarityMin = Math.min(similarityMin, similarity);
-        similarityMax = Math.max(similarityMax, similarity);
-
-        importanceMin = Math.min(importanceMin, memory.importance);
-        importanceMax = Math.max(importanceMax, memory.importance);
-
-        recencyMin = Math.min(recencyMin, recency);
-        recencyMax = Math.max(recencyMax, recency);
-
-        return {
-            ...memory,
+        WITH raw_memories AS (
+            SELECT 
+                id,
+                1 - (embedding <=> ${queryEmbeddingSql}::vector) AS raw_similarity,
+                importance AS raw_importance,
+                POWER(0.99, (EXTRACT(EPOCH FROM NOW() - "accessedAt") / 3600)) AS raw_recency
+            FROM "Memory"
+            WHERE "characterId" = ${character.id}
+        ),
+        min_max_values AS (
+            SELECT 
+                MIN(raw_similarity) AS similarityMin, MAX(raw_similarity) AS similarityMax,
+                MIN(raw_importance) AS importanceMin, MAX(raw_importance) AS importanceMax,
+                MIN(raw_recency) AS recencyMin, MAX(raw_recency) AS recencyMax
+            FROM raw_memories
+        ),
+        normalized_memories AS (
+            SELECT 
+                id,
+                (raw_similarity - similarityMin) / (similarityMax - similarityMin) AS similarity,
+                (raw_importance - importanceMin) / (importanceMax - importanceMin) AS importance,
+                (raw_recency - recencyMin) / (recencyMax - recencyMin) AS recency
+            FROM raw_memories, min_max_values
+        )
+        SELECT 
+            id,
             similarity,
+            importance,
             recency,
-        };
+            similarity + importance + recency AS normalized_score
+        FROM normalized_memories
+        ORDER BY normalized_score DESC
+        LIMIT ${top_k}
+    `) as {
+        id: string;
+        similarity: number;
+        importance: number;
+        recency: number;
+        normalized_score: number;
+    }[];
+
+    // Fetch the memories from the database
+    const memoriesFromDatabase = await prisma.memory.findMany({
+        where: {
+            id: {
+                in: memories.map((memory) => memory.id),
+            },
+        },
     });
-
-    // Normalize the memories
-    const memoriesWithNormalizedValues = memoriesWithSimilarity.map(
-        (memory) => {
-            const similarity =
-                (memory.similarity - similarityMin) /
-                (similarityMax - similarityMin);
-
-            const importance =
-                (memory.importance - importanceMin) /
-                (importanceMax - importanceMin);
-
-            const recency =
-                (memory.recency - recencyMin) / (recencyMax - recencyMin);
-
-            const normalizedScore = similarity + importance + recency;
-
-            return {
-                ...memory,
-                normalizedScore,
-            };
-        }
-    );
-
-    // Sort the memories by their normalized score
-    const memoriesSortedByNormalizedScore = memoriesWithNormalizedValues.sort(
-        (a, b) => {
-            return b.normalizedScore - a.normalizedScore;
-        }
-    );
-
-    // Return top k memories
-    const mostRelevantMemories = memoriesSortedByNormalizedScore.slice(
-        0,
-        top_k
-    );
 
     // If we don't want to update the accessedAt, return the memories
     if (!updateAccessedAt) {
-        return mostRelevantMemories;
+        return memoriesFromDatabase;
     }
 
     // Otherwise, update the returned memories' accessedAt values
     await Promise.all(
-        mostRelevantMemories.map((memory) =>
+        memoriesFromDatabase.map((memory) =>
             prisma.memory.update({
                 where: {
                     id: memory.id,
@@ -284,12 +232,11 @@ const getRelevantMemories = async (
         )
     );
 
-    return mostRelevantMemories;
+    return memoriesFromDatabase;
 };
 
 export {
     getMemoryImportance,
-    cosineSimilarity,
     createMemory,
     getLatestMemories,
     calculateMaxMemoriesForTask,
